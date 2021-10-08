3cd92b3b92d96636c70d0971fb3029c12b3c731b
Tom Wilson <twilson63@gmail.com>
2021-10-08 19:57:13 +0000

    updated dependencies #6

--- deps.js
+++ deps.js
@@ -1,2 +1,3 @@
 export { default as crocks } from "https://cdn.skypack.dev/crocks@0.12.4";
 export * as R from "https://cdn.skypack.dev/ramda@^0.27.1";
+export { deepSwap } from "https://x.nest.land/hyper-utils@0.0.2/deep-swap.js";
