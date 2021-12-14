954e9a19ed50679f221805cc9d7ce034dbaa4d06
Tyler Hall <tyler@hyper.io>
2021-12-14 13:20:43 -0500

    chore(deps): remove unused deepSwap #21

--- deps.js
+++ deps.js
@@ -1,3 +1,2 @@
 export { default as crocks } from "https://cdn.skypack.dev/crocks@0.12.4";
 export * as R from "https://cdn.skypack.dev/ramda@^0.27.1";
-export { deepSwap } from "https://x.nest.land/hyper-utils@0.0.3/deep-swap.js";
