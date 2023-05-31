4fc60d68c5c66d7479b4b362dca8cb9b7b38925a
Tyler Hall <tyler@hyper.io>
2023-05-31 18:43:10 +0000

    chore: bump deps

--- deps.js
+++ deps.js
@@ -4,4 +4,4 @@ export * as R from 'https://cdn.skypack.dev/ramda@0.28.0'
 export {
   HyperErr,
   isHyperErr,
-} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.0/packages/utils/hyper-err.js'
+} from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-utils%40v0.1.1/packages/utils/hyper-err.js'
