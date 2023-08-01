e5147bdd12b628cf784f4b178e955393d7f33512
Tyler Hall <tyler@hyper.io>
2023-08-01 18:46:40 +0000

    feat(indexDocuments): add passing partialFilter to create partial indexes

--- test/hyper.js
+++ test/hyper.js
@@ -1,5 +1,5 @@
-import { default as hyper } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.1.0/packages/core/mod.ts'
-import { default as app } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.1.0/packages/app-express/mod.ts'
+import { default as hyper } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.2.0/packages/core/mod.ts'
+import { default as app } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-app-express%40v1.2.0/packages/app-express/mod.ts'
 import { default as couchdb } from '../mod.js'
 
 hyper({
